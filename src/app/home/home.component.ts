f263c49672e5ade1531ee36927fdb66faa127070
vicvelaz <vicvelaz@ucm.es>
2025-03-29 21:53:52 +0100

    Add: Implement footer component and integrate it into the home layout; enhance header styles and add navigation overlay

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
