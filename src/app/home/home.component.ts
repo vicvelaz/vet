9dbf79d038bfc1a8cd17656f320ded080101d201
vicvelaz <vicvelaz@ucm.es>
2025-04-05 23:48:10 +0200

    Add: Integrate Angular Material components in header and footer; enhance layout and styles for improved UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatIconModule, MatFormFieldModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
