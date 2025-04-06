5e6edfb9ff740dcf0264094e83edab2ceed29c5a
vicvelaz <48315675+vicvelaz@users.noreply.github.com>
2025-04-06 17:13:59 +0200

    Add: Contact, Location, Services, and Timetable components

---
+++ src/app/sections/timetable/timetable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable',
+  imports: [],
+  templateUrl: './timetable.component.html',
+  styleUrl: './timetable.component.scss'
+})
+export class TimetableComponent {
+
+}
