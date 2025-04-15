719d83ff4c662cb369b436d3f4ebee829b88ccd1
vicvelaz <48315675+vicvelaz@users.noreply.github.com>
2025-04-15 20:55:31 +0200

    Feature/contact-location (#11)
    * Refactor footer and header styles; update global typography and layout for improved responsiveness
    
    * Update angular.json
    
    * Add: HeroComponent and data structure
    
    * Add: Contact, Location, Services, and Timetable components
    
    * Refactor: Update home layout and integrate new components for services, timetable, location, and contact sections
    
    * Refactor: Update footer, header, hero, services components to use dynamic data binding and improve layout
    
    * Refactor: Enhance service card layout and styling for improved responsiveness
    
    * Refactor: Organize section texts and improve styling for service cards
    
    * Fix: sticky header
    
    * Fix urls
    
    * Refactor hero section layout and styles for improved structure and readability
    
    * Add FontAwesome icons and update services section for improved UI
    
    * Add banner image to hero section and update styles for better presentation
    
    * minor changes
    
    * Refactor(location and timetable): sections to utilize data binding for dynamic content and improve structure with FontAwesome icons
    
    * refactor contact component to use FontAwesome icons
    
    * Add emergency hours
    
    * Refactor layout and styles for home, location, services, and timetable sections; update font size for better readability
    
    * Refactor styles for location, services, and timetable sections; unify background color and adjust layout properties
    
    * Refactor HTML structure for home component; change <div> to <main> for semantic correctness and update language attribute in index.html to Spanish
    
    * Add phone formatting pipe and update contact section layout
    
    - Implement PhoneFormatPipe for consistent phone number formatting.
    - Refactor contact.component.html and contact.component.scss for improved layout and styling.
    - Update home.component.ts with new promotional and insurance sections.
    - Adjust timetable styles for better responsiveness.
    
    * Add promotions and insurances sections to home component; implement respective components and templates
    
    * refactor folder structure
    
    * Implement promotions section with dynamic data and dialog functionality; add styles and dialog component
    
    * Refactor dialog styles and update dialog service to use custom dialog container
    
    * Refactor dialog functionality to pass image name; update dialog service and component to handle dynamic image data
    
    * Refactor insurances and promotions sections; update HTML structure, styles, and component logic for improved layout and functionality
    
    * Update header and location sections; enhance header link structure and replace banner image with Google Maps iframe
    
    * Feature/contact-location (#10)

---
+++ src/app/pages/legal-notice/legal-notice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss'
+})
+export class LegalNoticeComponent {
+
+}
