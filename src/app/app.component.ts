159f8931e2fdf0a04c1566cf814ba75a8aa37f6a
vicvelaz <vicvelaz@ucm.es>
2025-04-17 17:57:10 +0200

    Add @vercel/speed-insights dependency and update component templates for improved structure and accessibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { FooterComponent, HeaderComponent } from './components/layout';
 import { AppDataService } from './services/app-data.service';
 
@@ -16,6 +17,7 @@ export class AppComponent {
   data = signal<any>(null);
 
   ngOnInit() {
+    injectSpeedInsights();
     this.data.set(this.appDataService.data());
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
