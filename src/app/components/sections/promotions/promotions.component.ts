96633318dce025a7be1e0b14348c61d8dd36d2a4
vicvelaz <48315675+vicvelaz@users.noreply.github.com>
2025-07-06 00:20:23 +0200

    Feature/change date format and validation (#27)
    * Add date validation and formatting to promotions
    
    * Remove unused OnInit lifecycle hook and related date validation logic from PromotionsComponent

--- src/app/components/sections/promotions/promotions.component.ts
+++ src/app/components/sections/promotions/promotions.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, inject, input } from '@angular/core';
+import { AfterViewInit, Component, inject, input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { UtilsService } from '../../../services/utils.service';
 
@@ -12,6 +12,7 @@ export class PromotionsComponent implements AfterViewInit {
   readonly utilsService = inject(UtilsService);
 
   data = input.required<any>({});
+  today = signal(new Date());
 
   ngAfterViewInit(): void {
     const images = this.data().items.map((promotion: any) => 'img/' + promotion.image);
@@ -20,5 +21,13 @@ export class PromotionsComponent implements AfterViewInit {
 
   openDialog(imageName: string) {
     this.utilsService.openDialog('img/' + imageName);
+  }
+
+  isValidDate(promotionDate: string): boolean {
+    return new Date(promotionDate).setHours(0, 0, 0, 0) >= this.today().setHours(0, 0, 0, 0);
+  }
+
+  formatDate(date: string): string {
+    return new Date(date).toLocaleDateString();
   }
 }
