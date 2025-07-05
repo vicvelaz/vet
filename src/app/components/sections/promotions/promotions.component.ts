7e1b0264b6c4818b2cf8ecb7aafc517a3a44cffb
vicvelaz <vicvelaz@ucm.es>
2025-07-06 00:02:07 +0200

    Remove unused OnInit lifecycle hook and related date validation logic from PromotionsComponent

--- src/app/components/sections/promotions/promotions.component.ts
+++ src/app/components/sections/promotions/promotions.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, inject, input, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { UtilsService } from '../../../services/utils.service';
 
@@ -8,23 +8,11 @@ import { UtilsService } from '../../../services/utils.service';
   templateUrl: './promotions.component.html',
   styleUrl: './promotions.component.scss',
 })
-export class PromotionsComponent implements AfterViewInit, OnInit {
+export class PromotionsComponent implements AfterViewInit {
   readonly utilsService = inject(UtilsService);
 
   data = input.required<any>({});
   today = signal(new Date());
-
-  ngOnInit(): void {
-    this.data().items.forEach((promotion: any) => {
-      // promotion.date = new Date(promotion.date).toLocaleDateString();
-      // if (new Date(promotion.date).setHours(0, 0, 0, 0) < this.today().setHours(0, 0, 0, 0)) {
-      //   console.warn(`Promotion ${promotion.title} is expired: ${promotion.date}`);
-      //   console.log(this.today());
-      // } else {
-      //   console.log(`Promotion ${promotion.title} is valid until: ${promotion.date}`);
-      // }
-    });
-  }
 
   ngAfterViewInit(): void {
     const images = this.data().items.map((promotion: any) => 'img/' + promotion.image);
@@ -36,7 +24,6 @@ export class PromotionsComponent implements AfterViewInit, OnInit {
   }
 
   isValidDate(promotionDate: string): boolean {
-    console.log('Checking date:', new Date(promotionDate), this.today());
     return new Date(promotionDate).setHours(0, 0, 0, 0) >= this.today().setHours(0, 0, 0, 0);
   }
 
